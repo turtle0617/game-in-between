670a2f5450e64062a76bbdb751adf8b8003351a2
turtle.chang <turtle.chang@pinkoi.com>
2023-01-26 11:55:03 +0800

    misc: Install vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,21 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 const app = createApp(App);
 
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,13 @@
 
 <template>
   <main>
-    <h2>Home</h2>
+    <v-card class="mx-auto" width="400">
+      <template v-slot:title> This is a title </template>
+
+      <v-card-text>
+        <span>aa</span>
+        <v-icon icon="mdi-cards-spade" />
+      </v-card-text>
+    </v-card>
   </main>
 </template>
