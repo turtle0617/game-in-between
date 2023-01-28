d2f0a0b628035ce1a09dd6cef585e2061881793f
turtle.chang <turtle.chang@pinkoi.com>
2023-01-28 16:01:10 +0800

    fix(vite): Remove '.' in base flag

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./game-in-between",
+  base: "/game-in-between",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
